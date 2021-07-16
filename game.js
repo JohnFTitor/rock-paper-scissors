9eb636ecd3868a5bd00340877c94995b8d58fd67
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2021-07-15 20:01:58 -0500

    Set the basic structure for the gameMode and its styling, while also changed some functionality of the script, now toggling the view between home and gameMode, though now it's not enabled due to gameMode designing

--- game.js
+++ game.js
@@ -1,43 +1,48 @@
 const selections = ["rock", "paper", "scissors"];    
 
+//returns one out of the three options of the game using random
 function computerPlay() {
     let index = Math.floor(Math.random() * 3);
     return selections[index];
 }
 
+//determines the winner of a single round, comparing playerSelection
+//with computer selection
 function playRound(playerSelection, computerSelection) {
-    console.log("Computer Play: " + computerSelection);
+    alert("Computer Play: " + computerSelection);
     switch (playerSelection) {
         case computerSelection: 
-            console.log("It's a tie")
+            alert("It's a tie")
             return "tie";
         case "rock":
             if (computerSelection === 'scissors') {
-                console.log("You won! Rock beats Scissors");
+                alert("You won! Rock beats Scissors");
                 return "player";
             } else {
-                console.log("You lost. Paper beats Rock");
+                alert("You lost. Paper beats Rock");
                 return "computer";
             }
         case "paper":
             if (computerSelection === "scissors") {
-                console.log("You lost. Scissors beats Paper");
+                alert("You lost. Scissors beats Paper");
                 return "computer";
             } else {
-                console.log("You won! Paper beats Rock");
+                alert("You won! Paper beats Rock");
                 return "player";
             }
         case "scissors":
             if (computerSelection === "rock") {
-                console.log("You lost. Rock beats Scissors")
+                alert("You lost. Rock beats Scissors")
                 return "computer";
             } else {
-                console.log("You won! Scissors beats Paper")
+                alert("You won! Scissors beats Paper")
                 return "player";
             }    
     }
 }
 
+//EntryMode game, this function makes sure the player only enters a valid
+//selection and returns the selection lowercased.
 function selection() {
     let checked = false;
     let playerSelection = '';
@@ -48,28 +53,44 @@ function selection() {
     return playerSelection;    
 }
 
+//Creates a gameSet of five rounds and declares the winner of the match.
 function game() {
     let playerScore = 0;
     let computerScore = 0;
     for (let i = 0; i < 5; i++) {
         let playerSelection = selection();
-        console.log("Player Selection: " + playerSelection);
+        alert("Player Selection: " + playerSelection);
         winner = playRound(playerSelection, computerPlay());
         if (winner === "player") {
             playerScore += 1;
         } else if (winner === "computer") {
             computerScore += 1;
         }
+        //whenever one of them gets 3 victories, the game is finished.
         if (playerScore === 3 || computerScore === 3) {
             break;
         }
     }
     if (playerScore > computerScore) {
-        console.log("Player won! Score: " + playerScore);
+        alert("Player won! Score: " + playerScore);
     } else if (playerScore === computerScore) {
-        console.log("It's a tie. What a shame");    
+        alert("It's a tie. What a shame");    
     } else {
-        console.log("Computer won. Score: " + computerScore);
+        alert("Computer won. Score: " + computerScore);
     }
 }
+
+
+//Changes the view when start button pressed, entering the game
+function toggleView() {
+    if (home.style.display === "") {
+        home.style.display= "none";
+    }
+} 
+
+
+const home = document.getElementById("home");
+const startButton = document.getElementById("startButton");
+startButton.addEventListener("click", toggleView);
+
 
